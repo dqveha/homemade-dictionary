ae2c26cd0bf55a7c393adec777ba86181a69a5ed
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-23 09:25:03 -0700

    Work on Definition class and set up function to hold array within Definition class variable with a good fail

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,3 +1,18 @@
 class Definition
+  attr_reader
+  attr_accessor
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(insert_definition, inserted_word_id, id)
+    @insert_definition = insert_definition
+    @inserted_word_id = inserted_word_id
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all()
+    return
+  end
 
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,3 +2,13 @@ require 'rspec'
 require 'word'
 require 'definition'
 require 'pry'
+
+describe('#Definition') do
+
+  describe('.all') do
+    it("returns an empty array when there are no definitions") do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+
+end
